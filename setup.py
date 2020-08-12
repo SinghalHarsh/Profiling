bcdfa856b339def1d3478c57294dbe80821c195b
Harsh Singhal <harshsinghal726@gmail.com>
2020-08-12 16:12:55 +0530

    added requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 import setuptools
+from pathlib import Path
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# Read the contents of README file
+source_root = Path(".")
+with (source_root / "README.md").open(encoding="utf-8") as f:
+    long_description = f.read()
+
+# Read the requirements
+with (source_root / "requirements.txt").open(encoding="utf8") as f:
+    requirements = f.readlines()
 
 setuptools.setup(
     name='fds_profiling',  
@@ -10,7 +17,7 @@ setuptools.setup(
     description="Generate profile report",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    
+    install_requires=requirements,
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
     
